a6419e6cf36d9b18bd1c0e867a4e75caa61a4ab2
Dina Belova <dbelova@mirantis.com>
2014-10-31 16:02:36 +0300

    Add configurable structure of OpenTSDB client
    Now it's possible to choose one of two client implementations -
    either via REST API or via direct socket usage. Socket client
    currently supports only meter posting.
    
    Change-Id: I6005cfa23ffbe4bdba15e62637150e8a29bdefc2

--- opentsdbclient/tests/test_client.py
+++ opentsdbclient/tests/test_client.py
@@ -13,54 +13,16 @@
 # License for the specific language governing permissions and limitations
 # under the License.
 
-import json
-
-import mock
-import requests
-
 from opentsdbclient import client
+from opentsdbclient.rest import client as rest_cl
+from opentsdbclient.socket import client as socket_cl
 from opentsdbclient import tests
 
 
 class ClientTest(tests.BaseTestCase):
-    def setUp(self):
-        super(ClientTest, self).setUp()
-        self.client = client.OpenTSDBClient(opentsdb_host=self.host,
-                                            opentsdb_port=self.port)
+    def test_get_client(self):
+        r_cl = client.get_client((self.host, self.port), protocol='rest')
+        s_cl = client.get_client((self.host, self.port), protocol='socket')
 
-    @mock.patch.object(requests, 'get')
-    def test_get_statistics(self, get_mock):
-        self.client.get_statistics()
-        get_mock.assert_called_once_with('http://127.0.0.1:4242/api/stats')
-
-    @mock.patch.object(requests, 'post')
-    def test_put_meter(self, post_mock):
-        put_dict = {'metric': 'bla', 'timestamp': '0',
-                    'value': 123, 'tags': {'some_tag': 'foo'}}
-        self.client.put_meter(put_dict)
-        post_mock.assert_called_once_with(
-            'http://127.0.0.1:4242/api/put?details', data=json.dumps(put_dict))
-
-    @mock.patch.object(requests, 'post')
-    def test_define_retention(self, post_mock):
-        self.client.define_retention('foo', 12)
-        post_mock.assert_called_once_with(
-            'http://127.0.0.1:4242/api/uid/tsmeta?tsuid=foo',
-            data='{"tsuid": "foo", "retention": 12}')
-
-    @mock.patch.object(requests, 'get')
-    def test_get_aggregators(self, get_mock):
-        self.client.get_aggregators()
-        get_mock.assert_called_once_with(
-            'http://127.0.0.1:4242/api/aggregators')
-
-    @mock.patch.object(requests, 'get')
-    def test_get_version(self, get_mock):
-        self.client.get_version()
-        get_mock.assert_called_once_with('http://127.0.0.1:4242/api/version')
-
-    @mock.patch.object(requests, 'get')
-    def test_get_query(self, get_mock):
-        self.client.get_query('start=0&end=12&m=max:2-min:bla')
-        get_mock.assert_called_once_with(
-            'http://127.0.0.1:4242/api/query?start=0&end=12&m=max:2-min:bla')
+        self.assertIsInstance(r_cl, rest_cl.RESTOpenTSDBClient)
+        self.assertIsInstance(s_cl, socket_cl.SocketOpenTSDBClient)
